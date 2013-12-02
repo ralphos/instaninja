31e4a3bb9bcb8ea690ced739d638f73c9d9e7f46
Ralph Wintle <wintle.ralph@gmail.com>
2013-12-02 17:52:26 +0800

    Exception Notification for logging exceptions.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,18 @@ Instaninja::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+   # Mandrill configuration
+  config.action_mailer.default_url_options = { host: 'localhost:5000'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,26 @@ Instaninja::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+    # Mandrill configuration
+  config.action_mailer.default_url_options = { host: 'instaninja.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
+  }
+
+  # Send exceptions using Exception Notification
+  config.middleware.use ExceptionNotification::Rack,
+    :email => {
+      :email_prefix => "Instaninja - Exception",
+      :sender_address => %{"notifier" <wintle.ralph@gmail.com>},
+      :exception_recipients => %w{wintle.ralph@gmail.com}
+    }
 end
