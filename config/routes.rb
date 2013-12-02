c8b9e2f88b72627dadd6fcce28185d9a4e35b98f
Ralph Wintle <wintle.ralph@gmail.com>
2013-12-02 17:35:48 +0800

    Destroy hashtag.

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ Instaninja::Application.routes.draw do
   get '/auth/instagram/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy', as: :sign_out
 
-  delete '/hashtag/:id', to: 'hashtags#destroy', as: :destroy_hashtag
-
-  resources :hashtags, only: [:index, :create]
+  resources :hashtags, only: [:index, :create, :destroy]
   resources :users, only: [:update]
   resources :likes, only: [:create]
 end
