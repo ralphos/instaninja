2a840faa3e28a2fa59b0c1fa02a99e8ded794caa
Ralph Wintle <wintle.ralph@gmail.com>
2013-12-02 12:07:42 +0800

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Instaninja::Application.config.secret_key_base = 'eca925c9e402e8e5df85ba9300c0522719ef6c5060214ada33311f93e578d19e8ed1f46e67eda7c28c2795c3b160858503f50d4c89223d47d7e2678a7c206af9'
