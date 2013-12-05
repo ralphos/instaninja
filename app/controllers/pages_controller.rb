38a133ceb3c162e3b3909a6f4d5fb1b43e9e5bcc
Ralph Wintle <wintle.ralph@gmail.com>
2013-12-05 13:02:53 +0800

    http authenticate does not work with HV.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,6 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
 
-  before_action :authenticate
-
   def show
-  end
-
-  private
-  def authenticate
-    http_basic_authenticate_with name: 'instaninja', password: 'password'
   end
 end
