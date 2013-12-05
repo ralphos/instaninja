61ae703bb60754697a841a4f3988d8cc02890cce
Ralph Wintle <wintle.ralph@gmail.com>
2013-12-05 13:15:59 +0800

    Don't wrap http authenticate in a method.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,6 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
-
-  before_action :authenticate_for_production
-
-  private
-  def authenticate_for_production
-    if Rails.env.production?
-      http_basic_authenticate_with name: 'instaninja', password: 'password'
-    end
+  if Rails.env.production?
+    http_basic_authenticate_with name: 'instaninja', password: 'password'
   end
 end
