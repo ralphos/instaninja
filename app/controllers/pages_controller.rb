238fb21360d97983400ef9255716d5812e7a0ee4
Ralph Wintle <wintle.ralph@gmail.com>
2013-12-05 12:54:53 +0800

    Add basic http authentication to access production site.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,7 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
+
+  if Rails.env.production?
+    http_basic_authenticate_with name: 'instaninja', password: 'password'
+  end
 end
